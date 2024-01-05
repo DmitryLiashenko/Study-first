9aabf59bea24b8a1de60a839c939f67b0446b632
DmitryLiashenko <dmliash1@gmail.com>
2024-01-05 16:38:50 +0200

    Last Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# length = float(input("lenght"))
+# width = float(input("width"))
+# area = length*width
+# show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+# print(show)
+
+# name = "Dmitry"
+# age = 34
+# is_active = True
+# subscription = ()
+
+# name = input("Your name? ")
+# email = input("Your email? ")
+# age = int(input("Your age? "))
+# height = float(input("Your height? "))
+# is_active =  True
+
+#Списки
+# my_list = []
+# my_list = [2024, 3.12]
+# some_data = ['Python']
+# my_list.extend(some_data)
+# my_list.insert(1, 'Python')
+# my_list.reverse()
+# print(my_list)
+
+#Slovnik
+# data = {"year": 2024, "lang": "Python", "version": 3.12}
+
+# cat = {"nick": "Simon", "age": 7, "characteristics": ["лагідний", "кусається"]}
+# info = {"status": "vaccinated", "breed": True}
+# age = cat.get("age")
+# cat.update(info)
+# print(cat)
